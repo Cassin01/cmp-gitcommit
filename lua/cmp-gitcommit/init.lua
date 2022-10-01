e75487df2870c022f9db46e5476b9de704c942bd
Cassin01 <koyojb@gmail.com>
2022-10-01 19:50:17 +0900

    ff

--- lua/cmp-gitcommit/init.lua
+++ lua/cmp-gitcommit/init.lua
@@ -84,7 +84,7 @@ source.get_keyword_pattern = function()
 end
 
 source.complete = function(self, request, callback)
-  print(getline('.')[col('.')-1])
+  print(vim.fn.getline('.')[vim.fn.col('.')-1])
   if (request.context.option.reason == 'manual' and request.context.cursor.row == 1 and request.context.cursor.col == 1) or
     (request.context.option.reason == 'auto' and request.context.cursor.row == 1 and request.context.cursor.col == 2) then
     callback({
