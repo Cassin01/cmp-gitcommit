8967d36bd47cf65f86fbc77fe005e29c5fcf7f3c
Cassin01 <koyojb@gmail.com>
2022-10-01 17:20:19 +0900

    fix

--- lua/cmp-gitcommit/init.lua
+++ lua/cmp-gitcommit/init.lua
@@ -57,7 +57,7 @@ return [[\w\+]]
 end
 
 source.complete = function(self, request, callback)
-  if request.context.cursor.row ~= 1 or request.context.coursor.col ~= 1 then
+  if request.context.cursor.row ~= 1 or request.context.cursor.col ~= 1 then
     return callback()
   end
   local candidates = self:_get_candidates()
